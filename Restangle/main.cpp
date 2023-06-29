77c9d8fc2b09c32ddc562f7dca5c5b597e9666dc
urmsxlrx <138157646+urmsxlrx@users.noreply.github.com>
2023-06-29 23:28:19 +0300

    Add files via upload

--- Restangle/main.cpp
+++ Restangle/main.cpp
@@ -1,87 +1,106 @@
 #include <iostream>
-#include "Triangle.h"
+#include "Rectangle.h"
+
 
 using namespace std;
 
+class Rectangle {
+private:
+    double length;
+    double width;
 
+public:
+    Rectangle(double l, double w) {
+        length = l;
+        width = w;
+    }
 
+    double calculateArea() {
+        return length * width;
+    }
 
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
 
+    double calculateSide(double angle) {
+        return sqrt(pow(length, 2) + pow(width, 2) - 2 * length * width * cos(angle));
+    }
 
+    double calculateAngle() {
+        return acos((pow(length, 2) + pow(width, 2)) / (2 * length * width));
+    }
+};
 
-
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Rus");
-    Triangle tri1(0, 0, 0, 0);
+    Rectangle rect(0, 0);
     bool flag = false;
     unsigned int amount = 0;
+
     do {
         cout << "Выберите действие: " << endl;
-        cout << "0.Выйти из программы" << endl;
-        cout << "1.Посчитать площадь: " << endl;
-        cout << "2.Посчитать перемитр: " << endl;
-        cout << "3.Посчитать сторону: " << endl;
-        cout << "4.Посчитать угол: " << endl;
+        cout << "0. Выйти из программы" << endl;
+        cout << "1. Посчитать площадь" << endl;
+        cout << "2. Посчитать периметр" << endl;
+        cout << "3. Посчитать сторону" << endl;
+        cout << "4. Посчитать угол" << endl;
         cin >> amount;
-        switch (amount)
-        {
+
+        switch (amount) {
             case 0:
-            {
                 cout << "Работа программы успешно завершена" << endl;
                 flag = true;
                 break;
-            }
-            case 1:
-            {
-                double a = 0;
-                double h = 0;
-                cout << "Введите сторону 1: " << endl;
-                cin >> a;
-                cout << "Введите высоту: " << endl;
-                cin >> h;
-                cout << "Площадь равна: " << tri1.ploshad(a, h) << endl;
+
+            case 1: {
+                double l, w;
+                cout << "Введите длину: " << endl;
+                cin >> l;
+                cout << "Введите ширину: " << endl;
+                cin >> w;
+                rect = Rectangle(l, w);
+                cout << "Площадь равна: " << rect.calculateArea() << endl;
                 break;
             }
-            case 2:
-            {
-                double a, b, c;
-                cout << "Введите сторону 1: " << endl;
-                cin >> a;
-                cout << "Введите сторону 2: " << endl;
-                cin >> b;
-                cout << "Введите сторону 3: " << endl;
-                cin >> c;
-                cout << "Периметр равен: " << tri1.perimetr(a, b, c) << endl;
+
+            case 2: {
+                double l, w;
+                cout << "Введите длину: " << endl;
+                cin >> l;
+                cout << "Введите ширину: " << endl;
+                cin >> w;
+                rect = Rectangle(l, w);
+                cout << "Периметр равен: " << rect.calculatePerimeter() << endl;
                 break;
             }
-            case 3:
-            {
-                double a, b, c;
-                cout << "Введите сторону 1: " << endl;
-                cin >> a;
-                cout << "Введите сторону 2: " << endl;
-                cin >> b;
-                cout << "3 сторона равна: " << tri1.storona(a, b) << endl;
+
+            case 3: {
+                double l, w, angle;
+                cout << "Введите длину: " << endl;
+                cin >> l;
+                cout << "Введите ширину: " << endl;
+                cin >> w;
+                cout << "Введите угол (в радианах): " << endl;
+                cin >> angle;
+                rect = Rectangle(l, w);
+                cout << "Сторона равна: " << rect.calculateSide(angle) << endl;
                 break;
             }
-            case 4:
-            {
-                double a, b, c;
-                cout << "Введите сторону 1: " << endl;
-                cin >> a;
-                cout << "Введите сторону 2: " << endl;
-                cin >> b;
-                cout << "Введите сторону 3: " << endl;
-                cin >> c;
-                cout << "Угол равен: " << tri1.ugol(a, b, c) << endl;
+
+            case 4: {
+                double l, w;
+                cout << "Введите длину: " << endl;
+                cin >> l;
+                cout << "Введите ширину: " << endl;
+                cin >> w;
+                rect = Rectangle(l, w);
+                cout << "Угол равен: " << rect.calculateAngle() << " радиан" << endl;
                 break;
             }
         }
-    } while (flag != true);
-
+    } while (!flag);
 
     return 0;
 }
-
 
